ebe441707f9c61c4906efadeb79eaf130d633572
jpw0314 <sakurajpw@outlook.com>
2023-12-07 18:11:27 +0800

    1 初步完成UI设计

---
+++ vidoplay/videoclient/videoclientDlg.cpp
@@ -0,0 +1,204 @@
+﻿
+// videoclientDlg.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "videoclient.h"
+#include "videoclientDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CvideoclientDlg 对话框
+
+
+
+CvideoclientDlg::CvideoclientDlg(CWnd* pParent /*=nullptr*/)
+	: CDialog(IDD_VIDEOCLIENT_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+	m_status = false;
+}
+
+void CvideoclientDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_EDIT_PLAY, m_video);
+	DDX_Control(pDX, IDC_SLIDER_POS, m_pos);
+	DDX_Control(pDX, IDC_SLIDER_VOLUME, m_volume);
+	DDX_Control(pDX, IDC_EDIT_URL, m_url);
+	DDX_Control(pDX, IDC_BTN_PLAY, m_btnplay);
+}
+
+BEGIN_MESSAGE_MAP(CvideoclientDlg, CDialog)
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_WM_TIMER()
+	ON_WM_DESTROY()
+	ON_BN_CLICKED(IDC_BTN_PLAY, &CvideoclientDlg::OnBnClickedBtnPlay)
+	ON_BN_CLICKED(IDC_BTN_STOP, &CvideoclientDlg::OnBnClickedBtnStop)
+	ON_NOTIFY(TRBN_THUMBPOSCHANGING, IDC_SLIDER_POS, &CvideoclientDlg::OnTRBNThumbPosChangingSliderPos)
+	ON_NOTIFY(TRBN_THUMBPOSCHANGING, IDC_SLIDER_VOLUME, &CvideoclientDlg::OnTRBNThumbPosChangingSliderVolume)
+	ON_WM_HSCROLL()
+	ON_WM_VSCROLL()
+END_MESSAGE_MAP()
+
+
+// CvideoclientDlg 消息处理程序
+
+BOOL CvideoclientDlg::OnInitDialog()
+{
+	CDialog::OnInitDialog();
+
+	// 设置此对话框的图标。  当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	// TODO: 在此添加额外的初始化代码
+	SetTimer(0, 500, NULL);
+	m_pos.SetRange(0, 100); 
+	m_volume.SetRange(0, 100);
+	m_volume.SetTic(10);
+	m_volume.SetTicFreq(20);
+	SetDlgItemText(IDC_STATIC_VOLUME, _T("100%"));
+	SetDlgItemText(IDC_STATIC_TIME, _T("--:--:--/--:--:--"));
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。  对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void CvideoclientDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialog::OnPaint();
+	}
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR CvideoclientDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CvideoclientDlg::OnTimer(UINT_PTR nIDEvent)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值	
+	if (nIDEvent==0)
+	{
+		//控制层
+	}
+	CDialog::OnTimer(nIDEvent);
+}
+
+
+void CvideoclientDlg::OnDestroy()
+{
+	CDialog::OnDestroy();
+	KillTimer(0);
+	// TODO: 在此处添加消息处理程序代码
+}
+
+
+void CvideoclientDlg::OnBnClickedBtnPlay()
+{
+	if (m_status==false)
+	{
+		m_btnplay.SetWindowText(_T("暂停"));
+		m_status = true;
+		//TODO controller 的播放接口
+	}
+	else {
+		m_btnplay.SetWindowTextW(_T("播放"));
+		m_status = false;
+		// TODO: controller 的暂停接口
+	}
+	
+}
+
+
+void CvideoclientDlg::OnBnClickedBtnStop()
+{
+	m_btnplay.SetWindowText(_T("播放"));
+	m_status = false;
+	// TODO: controller 的停止接口
+}
+
+
+void CvideoclientDlg::OnTRBNThumbPosChangingSliderPos(NMHDR* pNMHDR, LRESULT* pResult)
+{
+	// 此功能要求 Windows Vista 或更高版本。
+	// _WIN32_WINNT 符号必须 >= 0x0600。
+	NMTRBTHUMBPOSCHANGING* pNMTPC = reinterpret_cast<NMTRBTHUMBPOSCHANGING*>(pNMHDR);
+	// TODO: 在此添加控件通知处理程序代码
+	TRACE("pos=%d  reason=%d \n", pNMTPC->dwPos, pNMTPC->nReason);
+	*pResult = 0;
+}
+
+
+void CvideoclientDlg::OnTRBNThumbPosChangingSliderVolume(NMHDR* pNMHDR, LRESULT* pResult)
+{
+	// 此功能要求 Windows Vista 或更高版本。
+	// _WIN32_WINNT 符号必须 >= 0x0600。
+	NMTRBTHUMBPOSCHANGING* pNMTPC = reinterpret_cast<NMTRBTHUMBPOSCHANGING*>(pNMHDR);
+	// TODO: 在此添加控件通知处理程序代码
+	TRACE("pos=%d  reason=%d \n", pNMTPC->dwPos, pNMTPC->nReason);
+	*pResult = 0;
+}
+
+
+void CvideoclientDlg::OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	TRACE("m_pos=%p m_volume=%p pScrollBar%p nSBCode=%d\n", &m_pos, &m_volume, pScrollBar, nSBCode);
+	if (nSBCode == 5)
+	{
+		CString strVolume;
+		strVolume.Format(_T("%d%%"), (nPos));
+		SetDlgItemText(IDC_STATIC_TIME, strVolume);
+	}
+	CDialog::OnHScroll(nSBCode, nPos, pScrollBar);
+}
+
+
+void CvideoclientDlg::OnVScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	TRACE("m_pos=%p m_volume=%p pScrollBar%p nSBCode=%d\n", &m_pos, &m_volume, pScrollBar,nSBCode);
+
+	if (nSBCode==5)
+	{
+		CString strVolume;
+		strVolume.Format(_T("%d%%"), (100 - nPos));
+		SetDlgItemText(IDC_STATIC_VOLUME, strVolume);
+	}
+	
+	CDialog::OnVScroll(nSBCode, nPos, pScrollBar);
+}
---
+++ vidoplay/videoclient/videoclientDlg.h
@@ -0,0 +1,47 @@
+﻿
+// videoclientDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CvideoclientDlg 对话框
+class CvideoclientDlg : public CDialog
+{
+// 构造
+public:
+	CvideoclientDlg(CWnd* pParent = nullptr);	// 标准构造函数
+	bool m_status;
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_VIDEOCLIENT_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+	afx_msg void OnDestroy();
+	CEdit m_video;
+	CSliderCtrl m_pos;
+	CSliderCtrl m_volume;
+	CEdit m_url;
+	afx_msg void OnBnClickedBtnPlay();
+	CButton m_btnplay;
+	afx_msg void OnBnClickedBtnStop();
+	afx_msg void OnTRBNThumbPosChangingSliderPos(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnTRBNThumbPosChangingSliderVolume(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
+	afx_msg void OnVScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
+};
